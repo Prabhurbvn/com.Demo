00d2bcd6dcf7041b25ba31e419049dfc1a6e11b1
Prabhurbvn <prabhurbvn@gmail.com>
2020-09-10 14:51:36 +0530

    Create FbLogin1.java
    2nd commit

---
+++ src/test/java/com/Demo/TestScript/FbLogin1.java
@@ -0,0 +1,45 @@
+package com.Demo.TestScript;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FbLogin1 {
+	
+	
+	WebDriver driver;
+	@BeforeTest
+	public void setUpChrome()
+	{
+		//System.setProperty("webdriver.chrome.driver", "D:\\SeleniumBackups\\Chromedriver83\\chromedriver.exe");
+		
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		driver.get("http://www.fb.com");
+		driver.manage().window().maximize();
+	}
+	
+	@Test
+	public void verifyTitle()
+	{
+		Assert.assertEquals(driver.getTitle(), "Facebook – log in or sign up");
+	}
+	
+	@Test
+	public void verifyUrl()
+	{
+		Assert.assertEquals(driver.getCurrentUrl(), "https://www.facebook.com/");
+	}
+	
+	@AfterTest
+	public void tearDown()
+	{
+		driver.close();
+	}
+
+}
